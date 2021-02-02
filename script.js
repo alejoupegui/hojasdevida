cd3ce3fd3250411eab23896075fda5eef809ff3e
alejoupegui <62628909+alejoupegui@users.noreply.github.com>
2021-02-02 13:15:41 -0500

    Update script.js

--- script.js
+++ script.js
@@ -9,5 +9,25 @@ function testGS(){
 	});
 }
 
-document.getElementById("btn").addEventListener("click",testGS());
+function addGS(){
+
+	const url = "https://script.google.com/macros/s/AKfycbzks0DnacpVPOKu28vOu0nUyTJWKvkThsCQ9yeODBtOMvxtGLI/exec";
+
+	fetch(url,{
+		method: 'POST',
+		mode: 'no-cors',
+		cache: 'no-cache',
+		//credentials: 'omit',
+		headers:{
+			'Content-Type': 'application/json'
+		},
+		redirect: 'follow',
+		//referrerPolicy: 'no-referrer',
+		body: JSON.stringify({NombreEstado:"Veronica"})
+	});	
+}
+
+document.getElementById("btn").addEventListener("click",testGS);
+
+document.getElementById("btn2").addEventListener("click",addGS);
 
